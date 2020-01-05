dbbcfc6d659394f9d33af8806148841e13fe93cf
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 17:47:35 -0300

    ASP.NET Core 3.1 + REST API + XML
    ASP.NET Core 3.1 + REST API + XML

---
+++ APITemperaturas/Controllers/ConversorTemperaturasController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using APITemperaturas.Models;
+
+namespace APITemperaturas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConversorTemperaturasController : ControllerBase
+    {
+        [HttpGet("Fahrenheit/{temperatura}")]
+        public object GetConversaoFahrenheit(double temperatura)
+        {
+            Temperatura dados = new Temperatura();
+            dados.ValorFahrenheit = temperatura;
+            dados.ValorCelsius =
+                Math.Round((temperatura - 32.0) / 1.8, 2);
+            dados.ValorKelvin = dados.ValorCelsius + 273.15;
+
+            return dados;
+        }
+    }
+}
